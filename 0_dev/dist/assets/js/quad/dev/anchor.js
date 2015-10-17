f1961e496d26037a35cb1fe5e1e4e58a169054eb
SuguruSasaki <ss0402@gmail.com>
2015-10-17 10:40:42 +0900

    Add anchor.js

---
+++ 0_dev/dist/assets/js/quad/dev/anchor.js
@@ -0,0 +1,26 @@
+$(function(exports) {
+    exports.quad = exports.quad || {};
+    var module = exports.quad;
+
+    /**
+     アンカーナビゲーション機能を実装します。
+     */
+     if($('.js-anchor-nav').length){
+        $('.js-anchor-nav').each(setupNavigation);
+     }
+
+     /**
+     *
+     * @param key
+     * @param value
+     */
+    function setupAnchorNavigation(key, value){
+        $(value).click(function(e){
+            e.preventDefault();
+            var href = $(this).attr("href");
+            var target = $(href === "#" || href === "" ? 'html' : href);
+            target.velocity("scroll", { duration: 600, easing: "ease" });
+            return false;
+        });
+    }
+});
